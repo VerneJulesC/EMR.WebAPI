fce86a5c22ca436f4656b3facdafd2574b659bd4
unknown <vernejulesc@gmail.com>
2023-12-21 10:21:17 +0800

    Update to 4.8

--- ehr/models/ehr.Designer.cs
+++ ehr/models/ehr.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\This PC\git\EMR.WebAPI\ehr\models\ehr.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\User\git\kk\EMR.WebAPI\ehr\models\ehr.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
